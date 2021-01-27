6e335693ea3043e9eff68b865695ea1c492dadcb
Shashwat Pandey <47330116+shashwattpandeyy@users.noreply.github.com>
2021-01-27 12:39:48 +0530

    added doubly linked list

---
+++ DataStructure and Algorithms/src/DoubleEndedList/Node.java
@@ -0,0 +1,13 @@
+package DoubleEndedList;
+public class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+
+    public Node(int data) {
+        this.data = data;
+    }
+    public void displayNode(){
+        System.out.print(this.data + " ");
+    }
+}
